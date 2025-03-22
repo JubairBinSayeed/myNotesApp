fa3d063a204ca62c91b769391dc509adfb44ae36
JubairBinSayeed <jubairbinsayeed@gmail.com>
2025-03-22 22:32:56 +0600

    few adjustments made...

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
           <div className="navbar bg-base-100 shadow-sm font-[Boldonse]">
             <div className="flex-1">
-              <a className="btn btn-ghost text-xl gap-0"> MyNotes<span className="text-yellow-500">App</span></a>
+              <Link to='/' className="btn btn-ghost text-xl gap-0"> MyNotes<span className="text-yellow-500">App</span></Link>
             </div>
             <div className="flex-none">
               <ul className="menu menu-horizontal px-1">
--- src/Components/Notes/Notes.jsx
+++ src/Components/Notes/Notes.jsx
@@ -3,7 +3,11 @@
 const Notes = () => {
     return (
         <div>
-            <h1 className="text-5xl font-bold text-center my-10">Notes</h1>            
+            <h1 className="text-5xl font-bold text-center my-10">Notes</h1>     
+            <fieldset className="fieldset">
+  <legend className="fieldset-legend">Page title</legend>
+  <input type="text" className="input" placeholder="My awesome page" />
+</fieldset>       
         </div>
     );
 };
