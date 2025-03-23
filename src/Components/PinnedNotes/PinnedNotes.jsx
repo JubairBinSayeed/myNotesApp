56419dfaa75e5c5629672f2c260277dd3d998481
JubairBinSayeed <jubairbinsayeed@gmail.com>
2025-03-23 22:49:53 +0600

    pinnednotes...

--- src/Components/PinnedNotes/PinnedNotes.jsx
+++ src/Components/PinnedNotes/PinnedNotes.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 const PinnedNotes = () => {
     return (
         <div>
-            <h1 className="text-5xl font-bold text-center my-10">PinnedNotes</h1>            
+            <h1 className="text-5xl font-bold text-center my-10">PinnedNotes</h1> 
+            <p className="text-center">There aren't any notes yet! Click here to make your <br />own presonalised <Link to="/notes"  className="font-bold text-blue-500 hover:text-blue-600 hover:underline
+            ">notes</Link></p>           
         </div>
     );
 };
