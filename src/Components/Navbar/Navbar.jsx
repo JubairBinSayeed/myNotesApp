9e6c91a1c0bfae5a438440ba25339af7b892640a
JubairBinSayeed <jubairbinsayeed@gmail.com>
2025-03-23 16:20:04 +0600

    register almost done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
+import register from "../../assets/register.png"
 
 const Navbar = () => {
     return (
@@ -8,11 +9,11 @@ const Navbar = () => {
               <Link to='/' className="btn btn-ghost text-xl gap-0"> MyNotes<span className="text-yellow-500">App</span></Link>
             </div>
             <div className="flex-none">
-              <ul className="menu menu-horizontal px-1">
+              <ul className="menu menu-horizontal px-1 items-center">
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'text-yellow-500  font-bold' : 'font-bold')}><span className="mr-5">Home</span></NavLink>
                 <NavLink to="/notes" className={({ isActive }) => (isActive ? 'text-yellow-500 font-bold' : 'font-bold')}><span className="mr-5">Notes</span></NavLink>
                 <NavLink to="/pinnednotes" className={({ isActive }) => (isActive ? 'text-yellow-500 font-bold' : 'font-bold')}><span className="mr-5">PinnedNotes</span></NavLink>
-                <NavLink to="/register" className={({ isActive }) => (isActive ? 'text-yellow-500 font-bold' : 'font-bold')}><span className="mr-5">Register</span></NavLink>
+                <img src={register} className="w-[50px]"  alt="" />
               </ul>
             </div>
           </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,28 @@
-
+import register from "../../assets/register-two.png"
 
 const Register = () => {
     return (
-        <div>
-            <h1 className="text-5xl font-bold text-center my-10">Register</h1>            
+        <div className="flex justify-center items-center mt-32" style={{ fontFamily: "'Winky Sans', sans-serif" }}>
+            <div className="  w-[500px] h-[600px] shadow-2xl rounded-2xl">
+            <img src={register} className="ml-52 mt-10"  alt="" />
+            <h1 className="text-center text-3xl font-bold flex justify-center ">Register Your Account</h1>
+            <div className="ml-14">
+            <label className="input validator w-96 my-5">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></g></svg>
+  <input type="input" required placeholder="Username" pattern="[A-Za-z][A-Za-z0-9\-]*" minlength="3" maxlength="30" title="Only letters, numbers or dash" />
+</label><br />
+<label className="input validator mb-5 w-96">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
+  <input type="email" placeholder="mail@site.com" required/>
+</label><br />
+<div className="validator-hint hidden">Enter valid email address</div>
+<label className="input validator w-96 mb-5">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle></g></svg>
+  <input type="password" required placeholder="Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
+</label><br />
+<button className="btn  w-96 bg-yellow-500 border-yellow-500   text-2xl ">Register</button>
+            </div>
+            </div>
         </div>
     );
 };
