d913be85f43080a286ce62b90ed95d9b8454cf3f
JubairBinSayeed <jubairbinsayeed@gmail.com>
2025-03-24 15:23:52 +0600

    main objective almost complete

--- src/Components/Notes/Notes.jsx
+++ src/Components/Notes/Notes.jsx
@@ -1,23 +1,50 @@
-
+import { useState } from "react";
 
 const Notes = () => {
+    const [title, setTitle] = useState("");
+    const [submittedTitle, setSubmittedTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [submittedDescription, setSubmittedDescription] = useState("");
+    const handleNoteForm = e => {
+        e.preventDefault();
+        setSubmittedTitle(title);
+        setSubmittedDescription(description);
+    };
     return (
         <div style={{fontFamily: "'Winky Sans', sans-serif" }}>
             <h1 className="text-5xl font-bold text-center my-10">Notes</h1>
             <h1 className="text-3xl font-bold text-yellow-500 text-center">Note Form</h1>
-            <p className="text-xl font-bold ml-[700px]">Note Title:</p>     
-                <div className="flex justify-center items-center">
-            <input type="text" placeholder="Title..." className="input w-[500px] rounded-full" />   
+            <form onSubmit={handleNoteForm}>
+                <p className="text-xl font-bold ml-[700px]">Note Title:</p>     
+                <div className="flex justify-center items-center mb-3">
+                    <input 
+                        type="text" 
+                        name="title"  
+                        placeholder="Title..." 
+                        className="input w-[500px] rounded-full" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />   
                 </div>
                 <p className="text-xl font-bold ml-[700px]">Note Description:</p>
                 <div className="flex justify-center items-center">
-            <input type="text" placeholder="Describe..." className="input w-[500px] rounded-full" />   
+                    <input 
+                    type="text" 
+                    name="description" 
+                    placeholder="Describe..." 
+                    className="input w-[500px] rounded-full" 
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />   
                 </div>
                 <div className="flex justify-center my-5">
                     <div className="border-2 p-1 rounded-lg border-warning">
-                    <button className="btn btn-warning">Done</button>
+                        <input className="btn btn-warning" type="submit" value="Done" />
                     </div>    
                 </div>
+                {submittedTitle && <p className="flex justify-center items-center">{submittedTitle}</p>}
+                {submittedDescription && <p className="flex justify-center items-center">{submittedDescription}</p>}
+            </form>
         </div>
     );
 };
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 import Banner from '../Components/Banner/Banner';
 import Navbar from '../Components/Navbar/Navbar';
 import Notes from '../Components/Notes/Notes';
-import PinnedNotes from '../Components/PinnedNotes/PinnedNotes';
 import Register from '../Components/Register/Register';
 import AboutUs from '../Components/AboutUs/AboutUs';
 import Login from '../Components/Login/Login';
