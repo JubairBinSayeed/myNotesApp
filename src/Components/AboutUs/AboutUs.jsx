e266737bc567066268c942bd306789dbb7215873
JubairBinSayeed <jubairbinsayeed@gmail.com>
2025-03-24 11:02:12 +0600

    explore done

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,9 +1,24 @@
-
-
+import { Link } from "react-router-dom";
+import about from "../../assets/about-us.png";
+import construction from "../../assets/web-construction.png"
 const AboutUs = () => {
     return (
         <div>
-            <h1 className="text-center font-bold text-3xl underline">This is About Us Page</h1>            
+            <div className="flex justify-center my-5">
+            <img src={about} className=" " alt="" />
+            </div>
+            <p className="text-center">Our page focuses on the belief in efficient note taking.This website is free for everyone.
+                <br /> to get the best user experience you can register <Link className="font-bold text-yellow-500 hover:text-yellow-600 hover:underline">here</Link>.</p>        
+            <h1 className=" text-5xl font-bold text-center my-10 font-[Boldonse]">NOTE: THE WEBSITE IS UNDER <span className="text-yellow-500">CONSTRUCTION</span></h1>
+            <div className="flex justify-center my-5">
+                <img src={construction}   alt="" />
+            </div> 
+            <div className="flex justify-center my-5">
+            <div className="border-2 p-1 rounded-lg border-warning">
+            <Link to="/"  className="btn btn-warning font-bold">Go Back</Link>
+            </div>    
+            </div>
+            
         </div>
     );
 };
--- src/Components/PinnedNotes/PinnedNotes.jsx
+++ src/Components/PinnedNotes/PinnedNotes.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
 
-
 const PinnedNotes = () => {
     return (
         <div>
             <h1 className="text-5xl font-bold text-center my-10">PinnedNotes</h1> 
-            <p className="text-center">There aren't any notes yet! Click here to make your <br />own presonalised <Link to="/notes"  className="font-bold text-blue-500 hover:text-blue-600 hover:underline
+            <p className="text-center">There aren't any notes yet! Click here to make your <br />own presonalised <Link to="/notes"  className="font-bold text-yellow-500 hover:text-yellow-600 hover:underline
             ">notes</Link></p>           
         </div>
     );
